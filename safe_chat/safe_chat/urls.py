91f0f0ec020978b4bf939c062864a0a3b808b4b0
Kunj-Modi <modi.kunj07@gmail.com>
2023-08-25 02:58:43 +0530

    Add authentication
    Add login, register and logout option.

--- safe_chat/safe_chat/urls.py
+++ safe_chat/safe_chat/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.login_page, name="login"),
+    path('register/', views.register, name="register"),
+    path('logout/', views.logout_page, name="logout"),
 ]
---
+++ safe_chat/safe_chat/views.py
@@ -0,0 +1,49 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+
+def login_page(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        if not User.objects.filter(username=username).exists():
+            messages.error(request, "Invalid username.")
+            return redirect("/login/")
+
+        user = authenticate(username=username, password=password)
+
+        if user is None:
+            messages.error(request, "Invalid password.")
+            return redirect("/login/")
+        else:
+            login(request, user)
+            return HttpResponse("Hello world!")
+
+    return render(request, "login.html")
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "User already taken.")
+            return redirect("/register/")
+
+        user = User.objects.create(username=username)
+        user.set_password(password)
+        user.save()
+
+        messages.info(request, "User created successfully.")
+        return redirect("/login/")
+    return render(request, "register.html")
+
+
+def logout_page(request):
+    logout(request)
+    return redirect("/login/")
