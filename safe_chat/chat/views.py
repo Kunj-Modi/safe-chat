0e6f773caf082caaba802df29c9a0e05ecf24cb2
Kunj-Modi <modi.kunj07@gmail.com>
2023-08-26 11:51:26 +0530

    Make templates for chat-list and chat.

--- safe_chat/chat/views.py
+++ safe_chat/chat/views.py
@@ -1,8 +1,7 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 @login_required(login_url="/login/")
 def index(request):
-    return HttpResponse("Chat<br><a href=\"/logout/\">Logout</a>")
+    return render(request, "chat.html")
